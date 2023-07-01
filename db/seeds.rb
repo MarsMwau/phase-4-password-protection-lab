c6ac48d96fc9811aa346185ca70b9d8a47e883c0
MarsMwau <marthamumbua16@gmail.com>
2023-07-02 02:33:26 +0300

    Complete Lab

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "johndoe", password_digest: "password123")
+User.create(username: "janedoe", password_digest: "password456")
